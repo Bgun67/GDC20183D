96194f1548ffcaf7c778df06c587016b6f2c8df3
Mackenzie <43626181+MackoEXLA@users.noreply.github.com>
2018-11-07 18:31:42 -0500

    Weapon Basics

---
+++ Assets/Scripts/GunBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour {
+    
+    enum WeaponType
+    {
+        Projectile, Raycast, Melee
+    }
+
+    GameObject _projectile;
+
+    [SerializeField] WeaponType _weaponType = WeaponType.Raycast;
+    
+    public void Shoot()
+    {
+        if (this.isActiveAndEnabled)
+            return;
+
+        if (_weaponType == WeaponType.Projectile)
+        {
+            ProjectileShoot();
+        }
+        else if (_weaponType == WeaponType.Raycast)
+        {
+            RaycastShoot();
+        }
+        else if (_weaponType == WeaponType.Melee)
+        {
+            MeleeShoot();
+        }
+    }
+
+    protected virtual void RaycastShoot()
+    {
+        
+    }
+
+    protected virtual void ProjectileShoot()
+    {
+
+    }
+
+    protected virtual void MeleeShoot()
+    {
+
+    }
+
+}
