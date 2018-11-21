0a9ea1130f9cb040919ceb2f52c6ce24392b76cb
Meep3692 <Meep3692@gmail.com>
2018-11-21 18:40:22 -0500

    Navigation in Scene
    Tbh, it's just unity navmesh but it works

---
+++ Assets/Scripts/NavTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour
+{
+
+	public GameObject target;
+	public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		agent.destination = target.transform.position;
+	}
+}
