988ffb583890c294770a22feec2b8b5b1e02e0c7
Meep3692 <meep3692@gmail.com>
2018-12-02 19:05:19 -0500

    Start of nav debug commands
    Not much of a commit but I just recovered from Unity's "Let's delete everything irrecoverably because you had the wrong thing selected"

--- Assets/Scripts/NavTest.cs
+++ Assets/Scripts/NavTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using IngameDebugConsole;
 
 public class NavTest : MonoBehaviour
 {
@@ -9,10 +10,16 @@ public class NavTest : MonoBehaviour
 	public GameObject target;
 	public NavMeshAgent agent;
 
+	//[ConsoleMethod("nav_set_target", "Set target of npc to head towards")]
+	public void SetTarget(GameObject target)
+	{
+		this.target = target;
+	}
+
 	// Use this for initialization
 	void Start()
 	{
-
+		DebugLogConsole.AddCommandInstance("nav_set_target", "Set target of npc to head towards", "SetTarget", this);
 	}
 
 	// Update is called once per frame
