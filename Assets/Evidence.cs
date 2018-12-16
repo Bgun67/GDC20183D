89e8ae3e544e8f917548415a839ebab031b195b2
Braden Gunther <bradengunther@cmail.carleton.ca>
2018-12-16 13:21:47 -0500

    Edits to the thermal object shader

---
+++ Assets/Evidence.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Evidence : MonoBehaviour {
+	public bool isVisible = true;
+	public Camera detectiveCamera;
+	public string evidenceDescription = "Fingerprint of Guy LeMan";
+
+	//I intend to get rid of this
+	void Start()
+	{
+		Activate(GameObject.FindObjectOfType<Camera>());
+	}
+	//Called when player snaps to UV Camera
+	public void Activate(Camera _camera)
+	{
+		detectiveCamera = _camera;
+	}
+	//if seen by a camera
+	void OnBecameVisible () {
+		//check if the camera is staring at the evidence;
+		InvokeRepeating("CheckLookDirection", 0f, 0.5f);
+	}
+	//if no longer seen by any camera
+	void OnBecameInvisible()
+	{
+		CancelInvoke("CheckLookDirection");
+	}
+	void CheckLookDirection()
+	{
+		//check angle between
+		if (Vector3.Angle(detectiveCamera.transform.forward, transform.position - detectiveCamera.transform.position) < 10f)
+		{
+			//check to make sure there is nothing in between
+			RaycastHit _hit;
+			if (Physics.Linecast(detectiveCamera.transform.position, transform.position, out _hit) && _hit.transform == transform)
+			{
+				if (_hit.distance < 7f)
+				{
+					//check distance
+					Examine();
+				}
+			}
+		}
+	}
+	void Examine()
+	{
+		Debug.Log(evidenceDescription);
+	}
+}
---
+++ Assets/Scripts/Thermal_Vision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thermal_Vision : MonoBehaviour
+{
+	public Camera mainCamera;
+	public Shader thermalObject;
+	public Shader thermalShader;
+	public GameObject[] hotObjects;
+	public float intensity;
+	[Range(0,1)]
+	public int heatCam;
+	public Material material;
+	public Texture2D texture;
+	// Use this for initialization
+	// Creates a private material used to the effect
+	void OnEnable()
+	{
+		mainCamera.SetReplacementShader(thermalShader, "RenderType");
+	}
+	void OnDisable()
+	{
+		mainCamera.ResetReplacementShader();
+	}
+
+	// Postprocess the image
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		
+		Graphics.Blit(source, destination, material);
+		material.SetFloat("_amplification", intensity);
+		material.SetTexture("_MainTex", texture);
+	}
+	
+
+}
